a21517beba774f3a921274461bfc8268397ed00e
Felix Paradis <felixparadis@gmail.com>
2021-03-23 11:07:49 -0400

    piratepx: refactor for a bit of elegance

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,10 @@
+<script>
+    const { page } = stores();
+</script>
 <footer>
     A project by <a target="_blank" rel="noopener" href="https://www.felixparadis.com/">Félix Paradis</a>
     — <a target="_blank" rel="noopener" href="https://www.felixparadis.com/contact/">Send feedback</a>
     — <a target="_blank" rel="noopener" href="https://github.com/FelDev/nicetimeline">View code</a>
     — <a target="_blank" rel="noopener" href="https://ko-fi.com/feldev">Buy me a coffee</a>
+    <img src="https://app.piratepx.com/ship?p=e9814aae-7a7e-4d9f-85e5-a5a22c3ee4d5&i={$page.path.replace('/','%2f')}" alt="" style="display:none;"/>
 </footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -137,7 +137,6 @@
   <Footer/>
 
 <!-- </PageTransition> -->
-<img src="https://app.piratepx.com/ship?p=e9814aae-7a7e-4d9f-85e5-a5a22c3ee4d5" alt="" style="display:none;"/>
 <script>
   // import PageTransition from "../components/PageTransition.svelte";
   import { onMount } from "svelte";
